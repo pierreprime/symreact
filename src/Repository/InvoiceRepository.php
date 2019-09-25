3a948d96399f14e7d108162ccbde7042fbc537ec
pierreprime <pierre.vermeil@gmail.com>
2019-09-26 00:02:44 +0200

    Fix chrono error

--- src/Repository/InvoiceRepository.php
+++ src/Repository/InvoiceRepository.php
@@ -23,16 +23,7 @@ class InvoiceRepository extends ServiceEntityRepository
 
     public function findNextChrono(User $user){
 
-       /* 
-        var_dump($this->createQueryBuilder('i')
-            ->select('i.chrono')
-            ->join('i.customer', 'c')
-            ->where("c.user = :user")
-            ->setParameter('user', $user)
-            ->orderBy('i.chrono', 'DESC')
-            ->setMaxResults(1)
-            ->getQuery()->getSQL());*/
-
+        try {
             return $this->createQueryBuilder("i")
                     ->select("i.chrono")
                     ->join("i.customer", "c")
@@ -42,34 +33,8 @@ class InvoiceRepository extends ServiceEntityRepository
                     ->setMaxResults(1)
                     ->getQuery()
                     ->getSingleScalarResult() + 1;
+        }catch (\Exception $e){
+            return 1;
+        }
     }
-
-    // /**
-    //  * @return Invoice[] Returns an array of Invoice objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('i')
-            ->andWhere('i.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('i.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Invoice
-    {
-        return $this->createQueryBuilder('i')
-            ->andWhere('i.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
